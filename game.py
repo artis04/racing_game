3e580f54afc3c1b7654952f50dc0ef2cb47fb322
artis04 <artis.davis@outlook.lv>
2020-04-04 23:30:37 +0300

    added two tracks

--- game.py
+++ game.py
@@ -7,16 +7,26 @@ f.close()
 
 
 pygame.init()
+# scr_width = 1650
+# scr_height = 927
+scr_width = 1350
+scr_height = 944
 
-win = pygame.display.set_mode((500, 500))
+
+win = pygame.display.set_mode((scr_width, scr_height))
 
 pygame.display.set_caption("Racing game")
 
+#background
+# background = pygame.image.load('images/racing rack/track1.png')
+background = pygame.image.load('images/racing rack/track2.png')
+
+
 x = 50
 y = 50
-width = 40
-height = 60
-vel = 5
+width = 16
+height = 20
+vel = 20
 
 run = True
 while run:
@@ -26,6 +36,20 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and x > vel:
+        x -= vel
+    if keys[pygame.K_RIGHT] and x < scr_width - width:
+        x += vel
+    if keys[pygame.K_UP] and y > 0:
+        y -= vel
+    if keys[pygame.K_DOWN] and y < scr_height - height:
+        y += vel
 
+    win.fill((0, 0, 0))
+    win.blit(background, (0, 0))
+    pygame.draw.rect(win, (255, 99, 71), (x, y, width, height))
+
+    pygame.display.update()
 
 pygame.quit()
