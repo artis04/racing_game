88ab5aeae88605c275c8e8a8db5ee6cff0ee7752
artis04 <artis.davis@outlook.lv>
2020-04-04 22:56:54 +0300

    making window visible

--- game.py
+++ game.py
@@ -1,3 +1,31 @@
+import pygame
+
+# adds about.txt file
 f = open("about.txt", "a+")
 f.write("hello to this game")
 f.close()
+
+
+pygame.init()
+
+win = pygame.display.set_mode((500, 500))
+
+pygame.display.set_caption("Racing game")
+
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+
+run = True
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+
+pygame.quit()
